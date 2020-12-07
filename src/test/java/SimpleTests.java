b540b9b39a8a0d70893c96c6aa274604fd71fdab
PodleshyySergey <60898756+PodleshyySergey@users.noreply.github.com>
2020-12-07 14:14:43 +0300

    First commit

---
+++ src/test/java/SimpleTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTests {
+
+    @Test
+    void successTest() {
+        assertEquals(2 * 2, 4);
+    }
+
+    @Test
+    void negativeTest() {
+        assertEquals(2 * 2, 5);
+
+    }
+
+}
